59d6c7d2667a315ac514b59fea8f8c14ef159f78
jinhu <1528606247@qq.com>
2024-07-28 14:01:59 +0800

    前端初始版本

--- yudada-frontend/openapi.config.ts
+++ yudada-frontend/openapi.config.ts
@@ -2,6 +2,6 @@ const { generateService } = require("@umijs/openapi");
 
 generateService({
   requestLibPath: "import request from '@/request'",
-  schemaPath: "http://120.46.208.17:8101/api/v2/api-docs",
+  schemaPath: "http://localhost:8101/api/v2/api-docs",
   serversPath: "./src",
 });
